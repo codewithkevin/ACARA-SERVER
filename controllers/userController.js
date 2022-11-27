770002eab80e1598f19c18d5131345d7444f2ceb
codewithkevin <oseikelvin709@gmail.com>
2022-11-27 23:16:55 +0000

    Update

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,9 @@
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 
-// const createToken = (_id) => {
-//   return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "30d" });
-// };
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "30d" });
+};
 
 //Login a user
 const loginUser = async (req, res) => {
